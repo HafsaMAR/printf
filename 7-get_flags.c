82e6e1457cfb46755ee922ba2459919a1978b6dc
HafsaMAR <marouche.hafsa@gmail.com>
2023-03-28 10:39:52 -0700

    flag

--- 7-get_flags.c
+++ 7-get_flags.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
