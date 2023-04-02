35747f33a430d424773cd5451a1af2f875970a8b
HafsaMAR <122831327+HafsaMAR@users.noreply.github.com>
2023-04-02 12:05:50 +0000

    Update 10-function_3.c

--- 10-function_3.c
+++ 10-function_3.c
@@ -1,96 +1,6 @@
 #include "main.h"
 
 /**
- * print_pointer - prints the value of a pointer
- * @types: list arguments
- * @buffer: buffer array to print
- * @flags:  calculates active flags
- * @width: width
- * @precision: precision
- * @size: size
- * Return: number of character printed
- */
-int print_pointer(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	char extra_c = 0, padd = ' ';
-	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
-	unsigned long num_addrs;
-	char map_to[] = "0123456789abcdef";
-	void *addrs = va_arg(types, void *);
-
-	UNUSED(width);
-	UNUSED(size);
-
-	if (addrs == NULL)
-		return (write(1, "(nil)", 5));
-
-	buffer[BUFF_SIZE - 1] = '\0';
-	UNUSED(precision);
-
-	num_addrs = (unsigned long)addrs;
-
-	while (num_addrs > 0)
-	{
-		buffer[ind--] = map_to[num_addrs % 16];
-		num_addrs /= 16;
-		length++;
-	}
-
-	if ((flags & F_ZERO) && !(flags & F_MINUS))
-		padd = '0';
-	if (flags & F_PLUS)
-		extra_c = '+', length++;
-	else if (flags & F_SPACE)
-		extra_c = ' ', length++;
-
-	ind++;
-
-	return (write_pointer(buffer, ind, length,
-		width, flags, padd, extra_c, padd_start));
-}
-
-
-/**
- * print_non_printable - prints ascii codes in hexa of non printable chars
- * @types: List arguments
- * @buffer: Buffer array to print
- * @flags:  Calculates active flags
- * @width: width
- * @precision: Precision
- * @size: Size
- * Return: Number of character printed
- */
-int print_non_printable(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	int i = 0, offset = 0;
-	char *str = va_arg(types, char *);
-
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-
-	if (str == NULL)
-		return (write(1, "(null)", 6));
-
-	while (str[i] != '\0')
-	{
-		if (is_printable(str[i]))
-			buffer[i + offset] = str[i];
-		else
-			offset += append_hexa_code(str[i], buffer, i + offset);
-
-		i++;
-	}
-
-	buffer[i + offset] = '\0';
-
-	return (write(1, buffer, i + offset));
-}
-
-/**
  * print_reverse - prints string in reverse.
  * @types: List arguments
  * @buffer: Buffer array to print
@@ -131,6 +41,45 @@ int print_reverse(va_list types, char buffer[],
 		count++;
 	}
 	return (count);
+}
+
+/**
+ * print_non_printable - prints ascii codes in hexa of non printable chars
+ * @types: List arguments
+ * @buffer: Buffer array to print
+ * @flags:  Calculates active flags
+ * @width: width
+ * @precision: Precision
+ * @size: Size
+ * Return: Number of character printed
+ */
+int print_non_printable(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i = 0, offset = 0;
+	char *str = va_arg(types, char *);
+
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+
+	while (str[i] != '\0')
+	{
+		if (is_printable(str[i]))
+			buffer[i + offset] = str[i];
+		else
+			offset += append_hexa_code(str[i], buffer, i + offset);
+
+		i++;
+	}
+
+	buffer[i + offset] = '\0';
+
+	return (write(1, buffer, i + offset));
 }
 
 /**
@@ -182,4 +131,54 @@ int print_rot13string(va_list types, char buffer[],
 		}
 	}
 	return (count);
+}
+
+/**
+ * print_pointer - prints the value of a pointer
+ * @types: list arguments
+ * @buffer: buffer array to print
+ * @flags:  calculates active flags
+ * @width: width
+ * @precision: precision
+ * @size: size
+ * Return: number of character printed
+ */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
+
+	UNUSED(width);
+	UNUSED(size);
+
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
+	{
+		buffer[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+	if (flags & F_PLUS)
+		extra_c = '+', length++;
+	else if (flags & F_SPACE)
+		extra_c = ' ', length++;
+
+	ind++;
+
+	return (write_pointer(buffer, ind, length,
+		width, flags, padd, extra_c, padd_start));
 }
